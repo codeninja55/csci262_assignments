2d9b3936421e8026f7e7ed71ee37554418aab606
Andrew Che <andrew@codeninja55.me>
2018-08-16 11:39:12 +1000

    Added main()

--- assignment01/Rainbow.c
+++ assignment01/Rainbow.c
@@ -1,4 +1,16 @@
-//
-// Created by codeninja on 16/08/2018.
-//
+/*********************************************************************
+ * CSCI262 - Assignment 01
+ * Rainbow.c - main() for assignment 01 execution
+ * Dinh Che | dbac496 | Last modified 2018.08.16
+ * Author: codeninja55
+ *********************************************************************/
 
+int main( int argc, const char* argv[] )
+{
+    // if ( argc == 2 ) {
+    //
+    // } else {
+    //
+    // }
+
+}
