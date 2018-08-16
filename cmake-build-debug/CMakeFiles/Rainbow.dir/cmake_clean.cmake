3a66e68f4be6ed33b4fabbe31f26bee2bcba9735
Andrew Che <andrew@codeninja55.me>
2018-08-16 11:37:55 +1000

    Added project dir required by CLion

---
+++ cmake-build-debug/CMakeFiles/Rainbow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Rainbow.dir/assignment01/Rainbow.c.o"
+  "../assignment01/Rainbow.pdb"
+  "../assignment01/Rainbow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Rainbow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
