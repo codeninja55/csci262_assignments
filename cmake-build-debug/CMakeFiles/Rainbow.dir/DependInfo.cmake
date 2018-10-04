3db889b8b318d93a1f73bea5ee4bec73e8d5bb74
Andrew Che <andrew@codeninja55.me>
2018-10-04 17:33:44 +1000

    Completed ass2

---
+++ cmake-build-debug/CMakeFiles/Rainbow.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/codeninja/Github/csci262_assignments/assignment01/Rainbow.c" "/home/codeninja/Github/csci262_assignments/cmake-build-debug/CMakeFiles/Rainbow.dir/assignment01/Rainbow.c.o"
+  "/home/codeninja/Github/csci262_assignments/assignment01/avltree.c" "/home/codeninja/Github/csci262_assignments/cmake-build-debug/CMakeFiles/Rainbow.dir/assignment01/avltree.c.o"
+  "/home/codeninja/Github/csci262_assignments/assignment01/helper.c" "/home/codeninja/Github/csci262_assignments/cmake-build-debug/CMakeFiles/Rainbow.dir/assignment01/helper.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "../assignment01"
+  "/usr/include/openssl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
